051fe770d550b02e1fe0a998e86b58804e747d67
Tomo <suba213@hotmail.com>
2024-07-20 12:51:59 +0200

    2nd push

--- src/main/java/com/docker/exercise/demoexercise/repository/Patient.java
+++ src/main/java/com/docker/exercise/demoexercise/repository/Patient.java
@@ -1,15 +1,13 @@
 package com.docker.exercise.demoexercise.repository;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 @Entity
+@Table(name = "patients")
 public class Patient {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String firstName;
     private String lastName;
--- src/main/java/com/docker/exercise/demoexercise/restcontroller/RestControllerExercise.java
+++ src/main/java/com/docker/exercise/demoexercise/restcontroller/RestControllerExercise.java
@@ -1,11 +1,15 @@
 package com.docker.exercise.demoexercise.restcontroller;
 
+import com.docker.exercise.demoexercise.exceptions.PatientNotFoundException;
 import com.docker.exercise.demoexercise.repository.Patient;
 import com.docker.exercise.demoexercise.service.PatientService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api")
@@ -18,6 +22,32 @@ public class RestControllerExercise {
     public ResponseEntity<HttpStatus> addPatient(@RequestBody Patient patient){
         patientService.addPatient(patient);
         return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+    @GetMapping("/show/all")
+    public ResponseEntity<List<Patient>> getAllPatients(){
+        List<Patient> patients = patientService.getAllPatients();
+        return new ResponseEntity<>(patients,HttpStatus.OK);
+    }
+
+    @DeleteMapping("delete/patient/{id}")
+    public ResponseEntity<HttpStatus> deletePatient(@PathVariable Long id){
+        patientService.deletePatientById(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    @PutMapping("/put/{id}")
+    public ResponseEntity<HttpStatus> updatePatient(@PathVariable Long id, @RequestBody Patient updatePatient){
+        Optional<Patient> optionalPatient = patientService.findPatientById(id);
+        if (optionalPatient.isPresent()){
+            Patient patient = optionalPatient.get();
+            patient.setFirstName(updatePatient.getFirstName());
+            patient.setLastName(updatePatient.getLastName());
+            patient.setMedicalExamination(updatePatient.getMedicalExamination());
+            patient.setDaysUntilMedicalExamination(updatePatient.getDaysUntilMedicalExamination());
+            patientService.addPatient(patient);
+            return new ResponseEntity<>(HttpStatus.OK);
+        } else throw new PatientNotFoundException("Patient not found");
     }
 
 }
